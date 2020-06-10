86044566139bcd00b65a83605ad0c9cfd0bc4ca8
Athar1988 <hakim.athar.88@gmail.com>
2020-06-10 09:08:28 +0100

    Ajouter une boucle

--- Projet.Mercredi/src/main/java/Mercredi/Essai/Message.java
+++ Projet.Mercredi/src/main/java/Mercredi/Essai/Message.java
@@ -5,7 +5,9 @@ public class Message {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Affichage A=new Affichage();
+for(int i=0;i<10;i++){
 A.AffichageMessage();
+}
 	}
 
 }
