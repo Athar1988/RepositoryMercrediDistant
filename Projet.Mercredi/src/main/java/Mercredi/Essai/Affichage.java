bc1fa0642582e4093c9c63d78b6dd04b6c7b928c
Athar1988 <hakim.athar.88@gmail.com>
2020-06-10 09:13:59 +0100

    Ajouter code methode "erreur" et une boucle

--- Projet.Mercredi/src/main/java/Mercredi/Essai/Affichage.java
+++ Projet.Mercredi/src/main/java/Mercredi/Essai/Affichage.java
@@ -5,4 +5,7 @@ public class Affichage {
 	public void AffichageMessage() {
 		System.out.println("NOtre code ici");
 	}
+	public void AffichageErreur() {
+		System.out.println("ce n'est pas un code");
+	}
 }
--- Projet.Mercredi/src/main/java/Mercredi/Essai/Message.java
+++ Projet.Mercredi/src/main/java/Mercredi/Essai/Message.java
@@ -5,8 +5,12 @@ public class Message {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Affichage A=new Affichage();
+Affichage B=new Affichage();
 for(int i=0;i<10;i++){
 A.AffichageMessage();
+}
+for(int i=0;i<10;i++){
+B.AffichageErreur();
 }
 	}
 
